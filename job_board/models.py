589bd5c6511facd53f9d9d39ce1529c8afe6055d
bkleinb78 <y2bk728@aol.com>
2023-11-01 00:14:27 -0500

    created the django project and added the job postings for job board

--- job_board/models.py
+++ job_board/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class JobPosting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    salery = models.IntegerField()
+    is_active = models.BooleanField(default=False)
